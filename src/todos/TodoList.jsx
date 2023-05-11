625fe2979ebc8b1af395cba02222c4e945875ba0
Sevo <betont.online@googlemail.com>
2023-05-12 00:24:05 +0200

    separate todos in 2 lists, done and not done in render section

--- src/todos/TodoList.jsx
+++ src/todos/TodoList.jsx
@@ -31,48 +31,97 @@ export default function TodoList({
     <div className="TodoList">
       <h2>TodoList</h2>
       <ul>
-        {todos.map((todo) => {
-          return (
-            <li
-              key={todo.id}
-              className="p-4 border border-sky-500 border-b-0 last:border-b text-xl"
-            >
-              <input
-                type="checkbox"
-                checked={todo.done}
-                onChange={(evt) => {
-                  /*evt.target.checked = !todo.done;
+        {todos
+          .filter((todo) => !todo.done)
+          .map((todo) => {
+            return (
+              <li
+                key={todo.id}
+                className="p-4 border border-red-500 border-b-0 last:border-b text-xl"
+              >
+                <input
+                  type="checkbox"
+                  checked={todo.done}
+                  onChange={(evt) => {
+                    /*evt.target.checked = !todo.done;
                   console.log(evt.target.checked);
                   if (typeof onDone === "function") {
                     onDone(todo.id, evt.target.checked);
                   }*/
-                  fireOnDone(todo.id, evt.target.checked);
-                }}
-              />
-              <span
-                onClick={() => {
-                  fireOnDone(todo.id, !todo.done);
-                }}
-                className={`mx-2 cursor-pointer ${
-                  todo.done ? "line-through" : ""
-                }`}
+                    fireOnDone(todo.id, evt.target.checked);
+                  }}
+                />
+                <span
+                  onClick={() => {
+                    fireOnDone(todo.id, !todo.done);
+                  }}
+                  className={`mx-2 cursor-pointer text-red-500 ${
+                    todo.done ? "line-through" : ""
+                  }`}
+                >
+                  #{todo.id} {todo.title}
+                </span>
+                <a
+                  className="text-lg text-red-500 hover:underline hover:text-red-700 transition-all"
+                  href="#"
+                  title={`[${removeText}] #${todo.id} ${todo.title}`}
+                  onClick={(evt) => {
+                    evt.preventDefault();
+                    removeTodoClickHandler(todo.id);
+                  }}
+                >
+                  [{removeText}]
+                </a>
+              </li>
+            );
+          })}
+      </ul>
+      <br />
+      <ul>
+        {todos
+          .filter((todo) => todo.done)
+          .map((todo) => {
+            return (
+              <li
+                key={todo.id}
+                className="p-4 border border-green-500 border-b-0 last:border-b text-xl"
               >
-                #{todo.id} {todo.title}
-              </span>
-              <a
-                className="text-lg text-sky-500 hover:underline hover:text-sky-700 transition-all"
-                href="#"
-                title={`[${removeText}] #${todo.id} ${todo.title}`}
-                onClick={(evt) => {
-                  evt.preventDefault();
-                  removeTodoClickHandler(todo.id);
-                }}
-              >
-                [{removeText}]
-              </a>
-            </li>
-          );
-        })}
+                <input
+                  type="checkbox"
+                  checked={todo.done}
+                  onChange={(evt) => {
+                    /*evt.target.checked = !todo.done;
+                  console.log(evt.target.checked);
+                  if (typeof onDone === "function") {
+                    onDone(todo.id, evt.target.checked);
+                  }*/
+                    fireOnDone(todo.id, evt.target.checked);
+                  }}
+                />
+                <span
+                  onClick={() => {
+                    fireOnDone(todo.id, !todo.done);
+                  }}
+                  className={`mx-2 cursor-pointer text-green-500 ${
+                    todo.done ? "line-through" : ""
+                  }`}
+                >
+                  #{todo.id} {todo.title}
+                </span>
+                <a
+                  className="text-lg text-green-500 hover:underline hover:text-green-700 transition-all"
+                  href="#"
+                  title={`[${removeText}] #${todo.id} ${todo.title}`}
+                  onClick={(evt) => {
+                    evt.preventDefault();
+                    removeTodoClickHandler(todo.id);
+                  }}
+                >
+                  [{removeText}]
+                </a>
+              </li>
+            );
+          })}
       </ul>
     </div>
   );
